3e222656f6d18404c06569c2a71e490de97df994
Benjamin Bauer <benjamin.bauer@gmail.com>
2024-03-13 18:01:39 -0700

    Replaced w/ solutions

--- src/main.py
+++ src/main.py
@@ -2,10 +2,8 @@ from textnode import TextNode
 
 
 def main():
-  print("Starting Site Generator...")
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
 
-  my_first_node = TextNode("This is some smaple text", "bold", "https://www.boot.dev")
-
-  my_first_node.repr()
 
 main()
